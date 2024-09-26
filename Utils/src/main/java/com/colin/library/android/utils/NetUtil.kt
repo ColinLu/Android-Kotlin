7c4b77177cf16915d720470d1bc63ba8beb3915e
Colin.Lu <945919945@qq.com>
2024-09-26 13:17:11 +0800

    网络工具类，移除无效代码

--- Utils/src/main/java/com/colin/library/android/utils/NetUtil.kt
+++ Utils/src/main/java/com/colin/library/android/utils/NetUtil.kt
@@ -4,16 +4,10 @@ import android.Manifest.permission
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkInfo
 import android.telephony.TelephonyManager
 import androidx.annotation.IntDef
 import androidx.annotation.RequiresPermission
 import com.colin.library.android.utils.helper.UtilHelper
-import java.net.InetAddress
-import java.net.NetworkInterface
-import java.net.SocketException
-import java.util.LinkedList
-import java.util.Locale
 
 /**
  * 作者： ColinLu
@@ -84,18 +78,18 @@ object NetUtil {
 
 
     /*判断当前网络是否是以太网*/
-    fun isEthernet(network: NetworkCapabilities): Boolean {
-        return network.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+    fun isEthernet(capabilities: NetworkCapabilities? = networkCapabilities): Boolean {
+        return capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ?: false
     }
 
     /*判断当前网络是否是Wifi*/
-    fun isWifi(network: NetworkCapabilities): Boolean {
-        return network.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+    fun isWifi(capabilities: NetworkCapabilities? = networkCapabilities): Boolean {
+        return capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ?: false
     }
 
     /*判断当前网络是否是移动网*/
-    fun isCellular(network: NetworkCapabilities): Boolean {
-        return network.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+    fun isCellular(capabilities: NetworkCapabilities? = networkCapabilities): Boolean {
+        return capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ?: false
     }
 
     /**
@@ -118,33 +112,12 @@ object NetUtil {
     }
 
 
-    @get:RequiresPermission(permission.ACCESS_NETWORK_STATE)
-    val isNetworkAvailable: Boolean
-        /**
-         * 检查当前网络是否可用
-         *
-         * @return
-         */
-        get() {
-            // 获取手机所有连接管理对象（包括对wi-fi,net等连接的管理）
-            val networkInfo = connectivityManager.allNetworkInfo
-            if (networkInfo.isEmpty()) return false
-            for (i in networkInfo.indices) {
-                // 判断当前网络状态是否为连接状态
-                if (networkInfo[i].state == NetworkInfo.State.CONNECTED) return true
-            }
-            return false
-        }
-
     @NetType
     @RequiresPermission(permission.ACCESS_NETWORK_STATE)
-    fun getNetType(networkInfo: NetworkCapabilities? = networkCapabilities): Int {
-
-        //获取失败
-        if (null == networkInfo) return NetType.NETWORK_NONE
-        if (isEthernet(networkInfo)) return NetType.NETWORK_ETHERNET
-        if (isWifi(networkInfo)) return NetType.NETWORK_WIFI
-        if (isCellular(networkInfo)) return NetType.NETWORK_MOBILE
+    fun getNetType(capabilities: NetworkCapabilities? = networkCapabilities): Int {
+        if (isEthernet(capabilities)) return NetType.NETWORK_ETHERNET
+        if (isWifi(capabilities)) return NetType.NETWORK_WIFI
+        if (isCellular(capabilities)) return NetType.NETWORK_MOBILE
         return NetType.NETWORK_NONE
     }
 
@@ -172,78 +145,5 @@ object NetUtil {
             val manager = telephonyManager
             if (manager.simState == TelephonyManager.SIM_STATE_READY) return manager.simOperatorName
             return null
-        }
-
-
-    /**
-     * 获取IP地址
-     *
-     *
-     * 需添加权限
-     * `<uses-permission android:name="android.permission.INTERNET"/>`
-     *
-     *
-     * @param useIPv4 是否用IPv4
-     * @return IP地址
-     */
-    @RequiresPermission(permission.INTERNET)
-    fun getIPAddress(useIPv4: Boolean): String {
-        try {
-            val nis = NetworkInterface.getNetworkInterfaces()
-            val adds = LinkedList<InetAddress>()
-            while (nis.hasMoreElements()) {
-                val ni = nis.nextElement()
-                // To prevent phone of xiaomi return "10.0.2.15"
-                if (!ni.isUp || ni.isLoopback) continue
-                val addresses = ni.inetAddresses
-                while (addresses.hasMoreElements()) {
-                    adds.addFirst(addresses.nextElement())
-                }
-            }
-            for (add in adds) {
-                if (!add.isLoopbackAddress) {
-                    val hostAddress = add.hostAddress
-                    val isIPv4 = (hostAddress?.indexOf(':') ?: 0) < 0
-                    if (useIPv4) if (isIPv4) return hostAddress
-                    else {
-                        val index = hostAddress?.indexOf('%') ?: 0
-                        return if (index < 0) hostAddress.uppercase(Locale.getDefault())
-                        else hostAddress?.substring(0, index)?.uppercase(Locale.getDefault())!!
-                    }
-                }
-            }
-        } catch (e: SocketException) {
-            L.log(e)
-        }
-        return ""
-    }
-
-    val broadcastIpAddress: String
-        /**
-         * Return the ip address of broadcast.
-         *
-         * @return the ip address of broadcast
-         */
-        get() {
-            try {
-                val nis = NetworkInterface.getNetworkInterfaces()
-                val adds = LinkedList<InetAddress>()
-                while (nis.hasMoreElements()) {
-                    val ni = nis.nextElement()
-                    if (!ni.isUp || ni.isLoopback) continue
-                    val ias = ni.interfaceAddresses
-                    var i = 0
-                    val size = ias.size
-                    while (i < size) {
-                        val ia = ias[i]
-                        val broadcast = ia.broadcast
-                        if (broadcast != null) return broadcast.hostAddress
-                        i++
-                    }
-                }
-            } catch (e: SocketException) {
-                L.log(e)
-            }
-            return ""
         }
 }
